ab87f721b09abe74ebfd8bc025fa3134d3348794
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-02 00:01:50 -0500

    submission code

---
+++ titanic-competitions/submission.py
@@ -0,0 +1,21 @@
+"""
+the file for the submsission for the kaggle competation 
+"""
+
+import pandas as pd 
+
+#cloud
+FILE_PATH_test_c = "/home/ubuntu/s3/titanic_dataset/test.csv"
+FILE_PATH_SUB_c = "/home/ubuntu/s3/titanic_dataset/sample_submission.csv"
+
+
+#local linux
+L_FILE_PATH_test_l = "/home/abhi/Datasets/titanic_dataset/test.csv"
+L_FILE_PATH_SUB_l = "/home/abhi/Datasets/titanic_dataset/sample_submission.csv"
+
+
+#local mac
+M_FILE_PATH_test_l = "/Users/abhi/Datasets/titanic_dataset/test.csv"
+M_FILE_PATH_SUB_l = "/Users/abhi/Datasets/titanic_dataset/sample_submission.csv"
+
+
