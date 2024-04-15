412311b4139a8cc744c50bbfe5d032d1a2a388e0
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-04-15 08:03:50 -0400

    Code

---
+++ mnist-dataset/data_exploration.py
@@ -0,0 +1,40 @@
+"""
+explore the data on mnist dataset 
+"""
+
+#imports 
+import pandas as pd 
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+#from xgboost.sklearn import XGBClassifier
+
+import torch as th 
+from torch import nn
+
+#local cloud
+FILE_PATH_train_c = "/home/ubuntu/s3/digit-recognizer/train.csv"
+PREDICTION_DATA_c = "/home/ubuntu/s3/digit-recognize/test.csv"
+
+
+#local ubuntu
+FILE_PATH_train_l = "/home/abhi/Datasets/digit-recognizer/train.csv"
+PREDICTION_DATA_l = "/home/abhi/Datasets/digit-recognizer/test.csv"
+
+
+#local mac 
+FILE_PATH_train_m = "/Users/abhi/Datasets/digit-recognizer/train.csv"
+PREDICTION_DATA_m = "/Users/abhi/Datasets/digit-recognizer/test.csv"
+
+
+df_train = pd.read_csv(FILE_PATH_train_m)
+df_predtion = pd.read_csv(PREDICTION_DATA_m)
+
+
+print(df_train)
+print(df_predtion)
