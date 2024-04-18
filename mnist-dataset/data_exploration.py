03dd05eb1092467287115bc6a8819046580236f9
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-04-18 01:22:14 -0400

    data exploration

--- mnist-dataset/data_exploration.py
+++ mnist-dataset/data_exploration.py
@@ -32,9 +32,9 @@ FILE_PATH_train_m = "/Users/abhi/Datasets/digit-recognizer/train.csv"
 PREDICTION_DATA_m = "/Users/abhi/Datasets/digit-recognizer/test.csv"
 
 
-df_train = pd.read_csv(FILE_PATH_train_m) #use this 
+df_train = pd.read_csv(FILE_PATH_train_l) #use this 
 
-df_predtion = pd.read_csv(PREDICTION_DATA_m)
+df_predtion = pd.read_csv(PREDICTION_DATA_l)
 
 
 #split into the train and the test dataset 
@@ -46,4 +46,22 @@ X = df_train.drop("label", axis= 1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 
-print(y_train)
+#print(X_train.head())
+#print(y_train.head())
+
+
+#image size is 28 X 28 as square root of 784 pixels 
+
+
+#take one row and stack one another 
+
+test = X_train.iloc[0][0:28]
+
+#zeros_array = np.zeros((1, 28))
+zeros_array = test.values
+zeros_array = zeros_array.reshape(1,28)
+
+
+"""
+make a array and assign the value in the array and then we can use that array to train the model 
+"""
